69e6e6c3fc55ce11d54fdb4eb66628e07ef48f51
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2019-01-03 04:28:19 +0530

    Code Completed after 4 hours of research

--- rpi_server/settings.py
+++ rpi_server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from darkflow.net.build import TFNet
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'server'
 ]
 
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+options = {
+        'model': '/Users/shri99/darkflow/cfg/yolo.cfg',
+        'load': '/Users/shri99/darkflow/bin/yolo.weights',
+        'config': '/Users/shri99/darkflow/cfg',
+    }
+
+tfnet = TFNet(options)
--- server/views.py
+++ server/views.py
@@ -1,5 +1,20 @@
+import numpy as np
+import time
+import cv2
+import os
+from rpi_server import settings
 from django.http import JsonResponse
+from rest_framework.views import APIView
 
 
-def detect(request):
-        return JsonResponse({"a":"a"})
+class Detect(APIView):
+
+    def post(self, request, *args, **kwargs):
+        stime = time.time()
+        res = {}
+        frame = cv2.imread(request.POST.get('frame'), cv2.IMREAD_COLOR)
+        results = settings.tfnet.return_predict(frame)
+        for result in zip(results):
+             res[result['label']] = result['confidence']
+        print('FPS {:.1f}'.format(1 / (time.time() - stime)))
+        return JsonResponse(res)
